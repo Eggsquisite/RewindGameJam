b8b92701d6209efcdd7df2bd3b22c966317a261e
Marc Sevilleja <marc@sevilayha.com>
2020-08-04 01:49:39 -0700

    Imported post-processing and UVRP,
    Included a cam clamp script

---
+++ Assets/Scripts/CamClamp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamClamp : MonoBehaviour
+{
+    [SerializeField] float min_X = 0f;
+    [SerializeField] float max_X = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, min_X, max_X),
+            transform.position.y,
+            transform.position.z);
+    }
+}
