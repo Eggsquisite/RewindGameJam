c459a158c7415ac3bfabece60ef0d2c837d46974
EliTheWolf <matt.carr@me.com>
2020-08-04 12:07:02 -0700

    Started to add rotation

---
+++ Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject : MonoBehaviour {
+    
+    public Vector3 velocity = new Vector3(0f,1f,0f);
+    public Vector3 rotation = new Vector3(0f,0f,0f);
+    private RewindableObject ro;
+    public bool recordPositions = true;
+    public bool recordRotations = false;
+    public int rewindVelocity = 10;
+    public float rewindAccel = 2f;
+
+    void Start() {
+        if (recordPositions || recordRotations) ro = new RewindableObject(gameObject.transform, recordPositions, recordRotations);
+        //ro = gameObject.GetComponent<RewindObject>();
+    }
+
+    /*private void OnEnable() {
+        if ()
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (RewindManager.isRewinding) {
+            if (recordPositions) {
+                transform.position = ro.RewindPosition(3);
+            }
+
+            if (recordRotations) {
+                transform.rotation = ro.RewindRotation(3);
+            }
+            //Transform tf = gameObject.GetComponent<RewindObject>().GetPreviousTransform();
+            /*Vector3 tf = transform.position; //ignore the RHS, it just keeps an initialized value
+            for (int i = 0; i < rewindVelocity; i++) {
+                tf = ro.GetPreviousPosition();
+            }
+
+            transform.position = tf;*/
+            //transform.rotation = tf.rotation;
+            Debug.Log("New position: " + transform.position);
+        }
+        else {
+            transform.Translate(Time.deltaTime * velocity);
+            transform.Rotate(rotation);
+            if (recordPositions || recordRotations) ro.Add();
+        }
+    }
+    
+    
+}
---
+++ Assets/Scripts/Rewind/RewindableObject.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindableObject {
+
+    private Transform transform;
+    private RecordedPositions rp;
+    private RecordedRotations rr;
+    public bool savePosition, saveRotation;
+
+    public RewindableObject(Transform transform, bool savePosition, bool saveRotation) {
+        this.transform = transform;
+        this.savePosition = savePosition;
+        this.saveRotation = saveRotation;
+        if (savePosition) rp = new RecordedPositions();
+        if (saveRotation) rr = new RecordedRotations();
+    }
+
+    public void Add() {
+        if (savePosition) rp.Add(transform.position);
+        if (saveRotation) rr.Add(transform.rotation);
+    }
+
+    public Vector3 RewindPosition(int amount) {
+        return rp.RewindBy(amount);
+    }
+
+    public Quaternion RewindRotation(int amount) {
+        return rr.RewindBy(amount);
+    }
+
+    /*
+    void Start() {
+        if (saveRotation) recordedRotation = new List<Quaternion>(1000);
+    }*/
+
+    /*
+    void Add() {
+        if (position == recordedPosition.Count) {
+            if (savePosition) recordedPosition.Add(transform.position);
+            if (saveRotation) recordedRotation.Add(transform.rotation);
+            position++;
+        }
+        else {
+            if (savePosition) recordedPosition.Insert(position, transform.position);
+            if (saveRotation) recordedRotation.Insert(position, transform.rotation);
+            position++;
+        }
+    }*/
+    
+    /*void FixedUpdate() {
+        if (!RewindManager.isRewinding) { //only record positions when not rewinding
+            if (position == recordedPosition.Count) {
+                recordedPosition.Add(gameObject.transform.position);
+                if (saveRotation)recordedRotation.Add(gameObject.transform.rotation);
+                position++;
+            }
+            else recordedPosition.Insert(position++, gameObject.transform.position);
+        }
+    }*/
+
+    /*
+    public TransformLite GetPreviousTransform() {
+        if (position > 0) position--;
+        if (savePosition && saveRotation) return new TransformLite(recordedPosition[position], recordedRotation[position]);
+        else if (savePosition) return new TransformLite(recordedPosition[position]);
+        else if (saveRotation) return new TransformLite(recordedRotation[position]);
+        //else return null;
+    }*/
+
+
+    /*
+    public Vector3 GetPreviousPosition() {
+        if (position > 0) return recordedPosition[--position];
+        else return recordedPosition[0];
+    }
+
+   //Only call this after calling GetPreviousPosition
+    public Quaternion GetPreviousRotation() {
+        if (!savePosition)
+        return recordedRotation[position];
+    }*/
+}
+
+public class RecordedPositions {
+    private List<Vector3> recordedPosition = new List<Vector3>(1000);
+    private int pos = 0;
+
+    public void Add(Vector3 position) {
+        if (pos == recordedPosition.Count) { recordedPosition.Add(position); pos++; }
+        else recordedPosition.Insert(pos++, position);
+    }
+    
+    public Vector3 RewindBy(int amount) {
+        if ((pos - amount) >= 0) pos -= amount;
+        return recordedPosition[pos];
+    }
+}
+
+public class RecordedRotations {
+    private List<Quaternion> recordedRotation = new List<Quaternion>(1000);
+    private int pos = 0;
+    
+    public void Add(Quaternion rotation) {
+        if (pos == recordedRotation.Count) { recordedRotation.Add(rotation); pos++; }
+        else recordedRotation.Insert(pos++, rotation);
+    }
+    
+    public Quaternion RewindBy(int amount) {
+        if ((pos - amount) >= 0) pos -= amount;
+        return recordedRotation[pos];
+    }
+}
