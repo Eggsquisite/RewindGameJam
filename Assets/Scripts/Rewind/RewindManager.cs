72e54be67a1f6dbbc3449c591a3b20c81f066923
EliTheWolf <matt.carr@me.com>
2020-08-05 12:11:59 -0700

    Junk

--- Assets/Scripts/Rewind/RewindManager.cs
+++ Assets/Scripts/Rewind/RewindManager.cs
@@ -16,6 +16,17 @@ public class RewindManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
+            if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            
+                //rm.RewindTime();
+                RewindManager.isRewinding = true;
+                Debug.Log("REWINDING TRUE");
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+                RewindManager.isRewinding = false;
+                Debug.Log("REWINDING FALSE");
+            }
         
     }
 }
