3d9679c692d1e37a1159a82bc57227bf4de039fc
EliTheWolf <matt.carr@me.com>
2020-08-05 16:39:23 -0700

    Added centrifugal forces

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,48 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    
+    const float GRAVITY = -1f;
+
+    [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private LayerMask interactiveLayer;
     [SerializeField] float moveSpeed = 40.0f;
     [SerializeField] float jumpForce = 15.0f;
+
+    public RewindManager rm;
     private Rigidbody2D rb;
+    private Collider2D feet;
+    private Animator anim;
+    private SpriteRenderer sp;
+    private Vector3 movement;
+    private GameObject projectileGO;
+
+    float axisInput = 0f;
+    bool endTrigger = false;
+
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        feet = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space) && (rb.velocity.y == 0f)) Jump();
-        RewindTime();
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space)) Jump();
+
+        if (IsOnProjectile()) AddProjectileForce();
+
+        if (!endTrigger)
+            RewindTime();
     }
 
     
     private void FixedUpdate() {
         Move();
-        
     }
 
     public void RewindTime() {
@@ -32,35 +51,103 @@ public class PlayerMovement : MonoBehaviour {
             //rm.RewindTime();
             RewindManager.isRewinding = true;
             Debug.Log("REWINDING TRUE");
+            //rb.velocity = new Vector3(0f, 0f, 0f);
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift)) {
             RewindManager.isRewinding = false;
             Debug.Log("REWINDING FALSE");
         }
     }
+    
 
 
+    private bool IsGrounded()
+    {
+        float extraHeightText = .1f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(feet.bounds.center, Vector2.down, feet.bounds.extents.y + extraHeightText, platformLayerMask);
+        Color rayColor;
 
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(feet.bounds.center, Vector2.down * (feet.bounds.extents.y + extraHeightText), rayColor);
+        //Debug.Log(raycastHit.collider);
 
-    private void Move() {
-       float axisInput = Input.GetAxis("Horizontal");
+        anim.SetBool("jump", !(raycastHit.collider != null));
+
+        return raycastHit.collider != null;
+    }
+
+    private bool IsOnProjectile() {
+        float extraHeightText = .1f;
+        RaycastHit2D hit = Physics2D.Raycast(feet.bounds.center, Vector2.down, feet.bounds.extents.y + extraHeightText, interactiveLayer);
+        if (hit.collider != null) {
+            projectileGO = hit.transform.parent.gameObject;
+            Debug.Log(projectileGO.name);
+            return true;
+        }
+        else return false;
+    }
+
+    private void Move()
+    {
+        axisInput = Input.GetAxis("Horizontal");
 
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
             axisInput = 0;
 
         if (axisInput == 0) {
-            rb.velocity = new Vector3(0f, rb.velocity.y);
+            if (anim.GetBool("run"))
+                anim.SetBool("run", false);
+
+            movement = new Vector3(0f, rb.velocity.y);
         }
         else {
-            rb.velocity = new Vector3(axisInput * moveSpeed, rb.velocity.y);
-        }
+            if (axisInput > 0)
+                sp.flipX = false;
+            else if (axisInput < 0)
+                sp.flipX = true;
+
+            if (!anim.GetBool("run"))
+                anim.SetBool("run", true);
+
+            movement = new Vector3(axisInput * moveSpeed, rb.velocity.y);
+        } 
+        
+        rb.velocity = movement;
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        //rb.velocity = new Vector2(rb.velocity.x, 0f);
         rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        //anim.SetBool("jump", true);
+        anim.SetBool("jump", true);
     }
 
+    private void AddProjectileForce() {
+        Vector2 r = transform.position - projectileGO.transform.position;
+        float angularVel = projectileGO.GetComponent<MoveableObject>().GetAngularVelocity();
+        rb.AddForce(rb.mass*angularVel*angularVel*r, ForceMode2D.Impulse);
+    }
+
+    private void Light()
+    {
+        if (!endTrigger)
+        {
+            anim.SetTrigger("light");
+            endTrigger = true;
+        }
+    }
+
+    public void LightRange(bool status)
+    {
+        if (status)
+            EndTrigger.onAction += Light;
+        else
+            EndTrigger.onAction -= Light;
+    }
 }
