3f8eeae83a72cf39f1ad4ab7e79caaaa0bf5e9d6
EliTheWolf <matt.carr@me.com>
2020-08-08 03:06:21 -0700

    Added a new level, not done yet tho

---
+++ Assets/Scripts/TorchTriggerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class TorchTriggerMovement : MonoBehaviour {
+    public GameObject objectToMove;
+    private MoveableObject mo;
+    
+    // Start is called before the first frame update
+    void Start() {
+        mo = objectToMove.GetComponent<MoveableObject>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+
+        Debug.Log("ENABLED");
+            mo.enableAutoMovement = true;
+            Debug.Log("Check:  " + mo.enableAutoMovement);
+            gameObject.GetComponent<TorchTriggerMovement>().enabled = false;
+        
+    }
+}
