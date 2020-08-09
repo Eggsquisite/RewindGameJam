8e9aeab97979f30808452cbc83a3bef8d171138d
Marc Sevilleja <marc@sevilayha.com>
2020-08-09 01:25:27 -0700

    Music singleton added

---
+++ Assets/Scripts/MusicSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSingleton : MonoBehaviour
+{
+    public static MusicSingleton Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            Debug.Log("dontdestroyonload: " + gameObject.name);
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
